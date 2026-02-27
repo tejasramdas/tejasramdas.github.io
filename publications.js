bd9d2a641f79c0cedd4eef956eca63a078af9e25
Tejas Ramdas <tejas.ramdas@gmail.com>
2026-02-27 12:06:32 -0500

    pubs

---
+++ publications.js
@@ -0,0 +1,110 @@
+const publications = [
+    {
+        title: "Optimizing a neuron for reliable dendritic subunit pooling",
+        authors: "<strong>Tejas Ramdas</strong>, Bartlett W. Mel",
+        venue: "Neuroscience",
+        year: 2022,
+        url: "https://doi.org/10.1016/j.neuroscience.2021.10.017"
+    }
+    ,
+    {
+        title: "Speech Representations and Phoneme Classification for Preserving the Endangered Language of Ladin",
+        authors: "Zane Durante*, Leena Mathur*, Eric Ye, Sichong Zhao, <b>Tejas Ramdas</b>, Khalil Iskarous",
+        venue: "Interspeech",
+        year: 2021,
+        url: "https://arxiv.org/abs/2108.12531"
+    }
+];
+
+const posters = [
+    {
+        title: "Design principles for memory, storage, and recall in noisy intracellular networks",
+        authors: "<b>Tejas Ramdas</b>*, John J. Vastola*, Samuel J. Gershman",
+        venue: "COSYNE",
+        year: 2025,
+        url: "https://static1.squarespace.com/static/6102ca347474c263c40150cd/t/6894fc7f5fb1aa4a65a2fc1e/1754594431324/Cosyne+2025+Main+Meeting+Program+Book.pdf"
+    },
+    {
+        title: "Refining a table-based approach to prediction of active dendritic responses",
+        authors: "Christopher R. Jewell, <b>Tejas Ramdas</b>, Bartlett W. Mel",
+        venue: "Society for Neuroscience",
+        year: 2022,
+        url: "https://www.abstractsonline.com/pp8/#!/10619/presentation/64858"
+    },
+    {
+        title: "When should, and how can, a dendrite provide sharp thresholding, despite natural input variability?",
+        authors: "<b>Tejas Ramdas</b>, Bartlett W. Mel",
+        venue: "Society for Neuroscience Global Connectome",
+        year: 2021,
+        url: "https://www.sfn.org/-/media/SfN/Documents/NEW-SfN/Meetings/Past-Meetings/SFN21_Connectome_Abstracts_PDFs.pdf"
+    },
+
+];
+
+const talks = [
+    {
+        title: "Optimizing a neuron for reliable dendritic subunit pooling",
+        authors: "<b>Tejas Ramdas</b>, Bartlett W. Mel",
+        venue: "MIT Picower Institute Symposium [Dendrites: Molecules, Structure, and Function]",
+        year: 2021,
+        url: "https://web.cvent.com/event/59993c36-6254-4b7d-80da-4fd3c9014813/summary"
+    },
+];
+
+const adventures = [
+    {
+        title: "Softrock",
+        authors: "<b>Tejas Ramdas</b>",
+        venue: "Colorado",
+        year: 2025,
+        url: "https://www.youtube.com/watch?v=TvXSwcF81As"
+    },
+    // {
+    //     title: "Vermont 100",
+    //     authors: "<b>Tejas Ramdas</b> et al.",
+    //     venue: "Vermont",
+    //     year: 2024,
+    //     // url: "https://www.strava.com/activities/15155289101"
+    // },
+    {
+        title: "North South Trail",
+        authors: "<b>Tejas Ramdas</b>",
+        venue: "Rhode Island",
+        year: 2024,
+        url: "https://fastestknowntime.com/route/north-south-trail-ri"
+    },
+    {
+        title: "Pacific Crest Trail",
+        authors: "<b>Tejas Ramdas</b> et al.",
+        venue: "California, Oregon, Washington",
+        year: 2021,
+        url: "https://www.youtube.com/watch?v=YadSxyo-qh4"
+    }
+];
+
+const teaching = [
+    {
+        title: "<b>NEURO 120</b>: Intro to Computational Neuroscience",
+        authors: "Kristina Penikis",
+        venue: "Harvard",
+        year: "2024, 2025, 2026"
+    },
+    {
+        title: "<b>NEURO 80</b>: Intro to Neuroscience",
+        authors: "Katie Quast, Nao Uchida",
+        venue: "Harvard",
+        year: 2025
+    },
+    {
+        title: "<b>NEUROBIO 306QC</b>: Quantitative Methods for Biologists",
+        authors: "Ella Batty",
+        venue: "Harvard",
+        year: "2022, 2023, 2025"
+    },
+    {
+        title: "<b>GENED 1125</b>: Artificial and Natural Intelligence",
+        authors: "Venki Murthy",
+        venue: "Harvard",
+        year: 2023
+    }
+];
